6f49b4cb9aabab657d0180a021652b0e65a3f7a7
soemirno.k <soemirno@gmail.com>
2008-05-10 19:17:45 +0200

    made registry responsible for creating and storing sessions

--- lib/application.rb
+++ lib/application.rb
@@ -10,13 +10,4 @@ class Application
     session.handle(req, res)
   end
 
-private
-
-  def create_new_session(res)
-    session = Session.new
-    session_key = @@sessions.register(session)
-    res.cookies << @@sessions.create_cookie("heresy", session_key)
-    return session
-  end
-
 end
