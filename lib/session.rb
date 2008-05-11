2ac26e01b83e3e6b11ba03a3377474d432d492fd
soemirno.k <soemirno@gmail.com>
2008-05-11 11:48:15 +0200

    refactored: extract methods

--- lib/session.rb
+++ lib/session.rb
@@ -10,17 +10,25 @@ class Session
   end
 
   def handle(req, res)
-    req.query.each do |k,v|
+    process_request(req)
+    build_response(res)
+  end
+
+private
+
+  def build_response(response)
+    html = Canvas.new(@callbacks)
+    @root.render_on(html)
+    response.body = html.string
+    response["Content-Type"] = "text/html"  
+  end
+  
+  def process_requestx(request)
+    request.query.each do |k,v|
       if callback = @callbacks.find(k)
         callback.call(v)
       end
-    end
-
-    html = Canvas.new(@callbacks)
-    @root.render_on(html)
-
-    res.body = html.string
-    res["Content-Type"] = "text/html"
+    end    
   end
-
+  
 end
